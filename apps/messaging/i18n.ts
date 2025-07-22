7a5aebe29efdb7ed44c105a2a4dc481903617405
Samuele Salvatico <samuele.salvatico@nearform.com>
2025-07-22 10:01:28 +0200

    feat: start adding messages files

---
+++ apps/messaging/i18n.ts
@@ -0,0 +1,19 @@
+import { notFound } from "next/navigation"
+import { getRequestConfig } from "next-intl/server"
+
+const locales = ["en", "ga"]
+
+export default getRequestConfig(async ({ locale }) => {
+  // Validate that the incoming `locale` parameter is valid
+  // biome-ignore lint/suspicious/noExplicitAny: needed
+  if (!locales.includes(locale as any)) notFound()
+
+  const applicationMessages = (await import(`./messages/${locale}.json`))
+    .default
+
+  return {
+    messages: {
+      ...applicationMessages,
+    },
+  }
+})
